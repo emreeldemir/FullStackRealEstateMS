68fd1bcf7c362611ceeeef9b2861e6f5b89d8728
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-06 15:04:56 +0300

    dbcontext and cors policiy added

--- FinalProject/RealEstate/RealEstate.API/Program.cs
+++ FinalProject/RealEstate/RealEstate.API/Program.cs
@@ -1,11 +1,29 @@
+using RealEstate.API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configurationManager = builder.Configuration;
 
 // Add services to the container.
+
+builder.Services.AddDbContext<RealEstateContext>(options =>
+    options.UseSqlServer(configurationManager.GetConnectionString("RealEstate")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -15,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
