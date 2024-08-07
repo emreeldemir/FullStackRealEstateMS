dbcadbe52ada03e9ec8d351cfed89bdd53cecf80
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-08 00:17:11 +0300

    seed admin and user migration created

---
+++ FinalProject/RealEstate/RealEstate.API/Migrations/20240807142731_seedAdminAndUser.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RealEstate.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedAdminAndUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "b5bd82c7-e990-4414-bb19-35db324353b0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "377a5402-751f-41c3-ac9c-66b64c6ee53f");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { 1, 0, "4b04143a-0f08-424f-a841-c772a66f4ca7", "admin@example.com", true, "Admin", "Admin", false, null, "ADMIN@EXAMPLE.COM", "ADMIN", "AQAAAAIAAYagAAAAEPJX8IywKDlC9O4sgJ6PwXimjzs+3tBuYwgqHVF4RVDC/+NqdlQSFP895IoypZoQJg==", null, false, "4c5c2861-1737-4ff9-acb0-8d008dacc1d3", false, "admin" },
+                    { 2, 0, "1cf469be-2191-4028-8c6c-ab6b3e7fb3c3", "user@example.com", true, "John", "Doe", false, null, "USER@EXAMPLE.COM", "USER", "AQAAAAIAAYagAAAAEBd+MHSLmqpRMySjW1G1J1dykCwh5KhJbaQf+fhACNe8Hay/4pUoWTlB2MiK9vxhmA==", null, false, "75783f7e-84c7-45db-a20e-c8f508e63c97", false, "user" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "e8dcd71a-76c2-4831-8602-4c826fa24bb8");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "68c33feb-a5c4-48f6-86b2-9c81feed9ed5");
+        }
+    }
+}
