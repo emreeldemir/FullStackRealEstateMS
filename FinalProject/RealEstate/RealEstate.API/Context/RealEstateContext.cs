28202f5a631af3b53fc354bfb51b68b5912b6910
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-07 15:53:07 +0300

    context updated for adding roles

--- FinalProject/RealEstate/RealEstate.API/Context/RealEstateContext.cs
+++ FinalProject/RealEstate/RealEstate.API/Context/RealEstateContext.cs
@@ -2,17 +2,18 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RealEstate.API.Entities.Identity;
+using System.Reflection.Emit;
 
 namespace RealEstate.API.Context
 {
     public class RealEstateContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
     {
-         
+
         public RealEstateContext(DbContextOptions<RealEstateContext> options) : base(options)
         {
         }
 
-        
+
         public DbSet<Entities.Property> Properties { get; set; }
         public DbSet<Entities.Type> Types { get; set; }
         public DbSet<Entities.Status> Statuses { get; set; }
@@ -25,6 +26,12 @@ namespace RealEstate.API.Context
 
             //builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "user" });
             base.OnModelCreating(builder);
+
+            // Seed roles with unique integer IDs as strings and ConcurrencyStamp
+            builder.Entity<ApplicationRole>().HasData(
+                new ApplicationRole { Id = 1, Name = "admin", NormalizedName = "ADMIN", ConcurrencyStamp = Guid.NewGuid().ToString() },
+                new ApplicationRole { Id = 2, Name = "user", NormalizedName = "USER", ConcurrencyStamp = Guid.NewGuid().ToString() }
+            );
 
             // Additional configurations
             builder.Entity<Entities.Property>()
