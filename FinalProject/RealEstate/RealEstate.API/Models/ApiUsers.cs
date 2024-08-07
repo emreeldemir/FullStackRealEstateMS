c571ac27009a71c1a20b70ed2631aab216f18534
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-07 15:23:17 +0300

    created static classes for testing jwt token and jwt settings implemented

---
+++ FinalProject/RealEstate/RealEstate.API/Models/ApiUsers.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate.API.Models
+{
+    public class ApiUsers
+    {
+        public static List<ApiUser> Users = new()
+        {
+            new ApiUser { Id = 1, UserName = "emre", Password = "emre123456", Role = "User" },
+            new ApiUser { Id = 2, UserName = "deniz", Password = "deniz789", Role = "Admin" }
+        };
+        
+    }
+}
