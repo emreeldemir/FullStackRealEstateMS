ea1ce2d6a268ab67667f15dbac839b1a396bbee4
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-07 15:24:37 +0300

    jwt configs implemented and auth button on swagger

--- FinalProject/RealEstate/RealEstate.API/Program.cs
+++ FinalProject/RealEstate/RealEstate.API/Program.cs
@@ -1,18 +1,86 @@
 using RealEstate.API.Context;
 using Microsoft.EntityFrameworkCore;
+using RealEstate.API.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using RealEstate.API.Models;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configurationManager = builder.Configuration;
 
+
 // Add services to the container.
 
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configurationManager["Jwt:Issuer"],
+            ValidAudience = configurationManager["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configurationManager["Jwt:Key"] ?? string.Empty))
+        };
+    });
+
+// Read jwt settings and inject it to the all controllers
+builder.Services.Configure<JwtSettings>(configurationManager.GetSection("Jwt"));
+
+// DB Connection
 builder.Services.AddDbContext<RealEstateContext>(options =>
     options.UseSqlServer(configurationManager.GetConnectionString("RealEstate")));
+
+// .NET Core Identity Configuration
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    // Kimlik doðrulama seçeneklerini burada yapýlandýracam
+    options.SignIn.RequireConfirmedAccount = true;
+})
+.AddEntityFrameworkStores<RealEstateContext>()
+.AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RealEstateAPI", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. 
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddCors(options =>
 {
@@ -36,6 +104,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("AllowReactApp");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
