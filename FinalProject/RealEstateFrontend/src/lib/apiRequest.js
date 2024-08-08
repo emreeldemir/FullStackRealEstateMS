dbeffd248c9102b489abec2bde6072f95b6e392a
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-08 17:34:44 +0300

    interceptor for adding jwt into header and backend base url config

--- FinalProject/RealEstateFrontend/src/lib/apiRequest.js
+++ FinalProject/RealEstateFrontend/src/lib/apiRequest.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "http://localhost:5104/api",
   withCredentials: true,
+});
+
+apiRequest.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default apiRequest;
