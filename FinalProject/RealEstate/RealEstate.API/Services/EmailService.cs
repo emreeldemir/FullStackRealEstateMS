dfd21d3d621fb545f76443f8816c24710aa6299d
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-08 16:45:06 +0300

    email service implemented as a draft, not working

---
+++ FinalProject/RealEstate/RealEstate.API/Services/EmailService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace RealEstate.API.Services
+{
+    public class EmailService
+    {
+        private readonly string _smtpServer;
+        private readonly int _smtpPort;
+        private readonly string _smtpUser;
+        private readonly string _smtpPass;
+
+        public EmailService(string smtpServer, int smtpPort, string smtpUser, string smtpPass)
+        {
+            _smtpServer = smtpServer;
+            _smtpPort = smtpPort;
+            _smtpUser = smtpUser;
+            _smtpPass = smtpPass;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            using (var smtpClient = new SmtpClient(_smtpServer, _smtpPort))
+            {
+                smtpClient.Credentials = new NetworkCredential(_smtpUser, _smtpPass);
+                smtpClient.EnableSsl = true;
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(_smtpUser),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true
+                };
+                mailMessage.To.Add(to);
+
+                await smtpClient.SendMailAsync(mailMessage);
+            }
+        }
+    }
+}
