236fd57c3863c8950140bd0261d7b55d366f7c8b
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-12 01:10:50 +0300

    PropertySearchRequestDTO added for filtering

---
+++ FinalProject/RealEstate/RealEstate.API/DTOs/Property/PropertySearchRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.API.DTOs.Property
+{
+    public class PropertySearchRequestDTO
+    {
+        public string? Status { get; set; }
+        public string? Type { get; set; }
+        public string? Currency { get; set; }
+        public int? MinPrice { get; set; }
+        public int? MaxPrice { get; set; }
+    }
+}
