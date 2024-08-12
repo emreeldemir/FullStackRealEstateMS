0d52707d8a61e8c25107d8afb2a7ff855258266f
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-12 19:40:46 +0300

    card css updated

--- FinalProject/RealEstateFrontend/src/components/card/Card.jsx
+++ FinalProject/RealEstateFrontend/src/components/card/Card.jsx
@@ -2,43 +2,73 @@ import { Link } from "react-router-dom";
 import "./card.scss";
 
 function Card({ item }) {
-  console.log(item);
-  return (
+  // Fiyatı formatlama fonksiyonu
+  const formatPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
 
+  return (
     <div className="card">
       <Link to={`/${item.id}`} className="imageContainer">
-        <img src={item.photos[0].photoData} alt="" />
+        <img src={item.photos[0]?.photoData} alt="" />
       </Link>
+
       <div className="textContainer">
+        {/* Başlık */}
         <h2 className="title">
           <Link to={`/${item.id}`}>{item.title}</Link>
         </h2>
-        <p className="address">
-          <img src="/pin.png" alt="" />
+
+        {/* Açıklama */}
+        <p className="description">
           <span>{item.description}</span>
         </p>
-        <p className="price">$ {item.price}</p>
-        <div className="bottom">
-          {/* <div className="features">
-            <div className="feature">
-              <img src="/bed.png" alt="" />
-              <span>{item.bedroom} bedroom</span>
-            </div>
-            <div className="feature">
-              <img src="/bath.png" alt="" />
-              <span>{item.bathroom} bathroom</span>
-            </div>
-          </div> */}
-          <div className="icons">
-            <div className="icon">
-              <img src="/save.png" alt="" />
-            </div>
 
+        {/* Fiyat ve Para Birimi */}
+        <p className="price">
+          {formatPrice(item.price)} {item.currencyName}
+        </p>
+
+        {/* Tür ve Durum (Alt alta yazılması) */}
+        {/* <p className="feature">
+          <span>{item.typeName}</span>
+          <br />
+          <span>{item.statusName}</span>
+        </p> */}
+
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+
+              <span>{item.typeName}</span>
+            </div>
+            <div className="feature">
+
+              <span>{item.statusName}</span>
+            </div>
           </div>
         </div>
+
+        <div className="bottom">
+          <div className="features">
+            <div className="listedByContainer">
+              <span className="listedBy">Listed by:</span>
+              <span className="userName">{item.userName}</span>
+            </div>
+            <div className="icons">
+              <div className="icon">
+                <img src="/save.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
 }
 
 export default Card;
+
+
+
