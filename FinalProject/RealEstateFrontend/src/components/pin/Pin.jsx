248256a6346f74759eefe98784394da80ffd24f7
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-12 19:42:32 +0300

    leaflet pin parameters implemented

--- FinalProject/RealEstateFrontend/src/components/pin/Pin.jsx
+++ FinalProject/RealEstateFrontend/src/components/pin/Pin.jsx
@@ -3,15 +3,20 @@ import "./pin.scss";
 import { Link } from "react-router-dom";
 
 function Pin({ item }) {
+  // Fiyatı formatlama fonksiyonu
+  const formatPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
   return (
     <Marker position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupContainer">
-          <img src={item.images[0]} alt="" />
+          <img src={item.photos[0]?.photoData} alt="" />
           <div className="textContainer">
             <Link to={`/${item.id}`}>{item.title}</Link>
-            <span>{item.bedroom} bedroom</span>
-            <b>$ {item.price}</b>
+            {/* Fiyatı formatlayarak gösteriyoruz */}
+            <b>$ {formatPrice(item.price)}</b>
           </div>
         </div>
       </Popup>
