b316fd9c1745c4978ac5ec1d748f79628af32b79
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-14 01:12:38 +0300

    create new post implemented

--- FinalProject/RealEstateFrontend/src/lib/loaders.js
+++ FinalProject/RealEstateFrontend/src/lib/loaders.js
@@ -1,11 +1,16 @@
 import { defer } from "react-router-dom";
+import { useContext } from "react";
 import apiRequest from "./apiRequest";
+import { AuthContext } from "../context/AuthContext";
+
+
 
 export const singlePageLoader = async ({ request, params }) => {
   const res = await apiRequest("/posts/" + params.id);
   console.log(res);
   return res.data;
 };
+
 export const listPageLoader = async ({ request, params }) => {
   console.log(request.url);
   const query = request.url.split("?")[1];
@@ -17,10 +22,11 @@ export const listPageLoader = async ({ request, params }) => {
 };
 
 export const profilePageLoader = async () => {
-  const postPromise = apiRequest("/users/profilePosts");
+  const promise = apiRequest("/Property/GetAllProperties");
+  const filteredResults = promise.filter(result => result.userId === 8);
 
   return defer({
-    postResponse: postPromise,
+    postResponse: filteredResults,
 
   });
 };
--- FinalProject/RealEstateFrontend/src/routes/newPostPage/newPostPage.jsx
+++ FinalProject/RealEstateFrontend/src/routes/newPostPage/newPostPage.jsx
@@ -6,47 +6,67 @@ import apiRequest from "../../lib/apiRequest";
 import UploadWidget from "../../components/uploadWidget/UploadWidget";
 import { useNavigate } from "react-router-dom";
 import { DropdownContext } from "../../context/DropdownContext";
+import { AuthContext } from "../../context/AuthContext";
 
 function NewPostPage() {
   const { types, statuses, currencies } = useContext(DropdownContext);
+  const { currentUser } = useContext(AuthContext);
   const [value, setValue] = useState("");
   const [images, setImages] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "",
+    price: "",
+    currency: 1,
+    type: 1,
+    status: 1,
+    latitude: "",
+    longitude: "",
+  });
   const [error, setError] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const inputs = Object.fromEntries(formData);
 
     try {
-      const res = await apiRequest.post("/posts", {
-        postData: {
-          title: inputs.title,
-          price: parseInt(inputs.price),
-          address: inputs.address,
-          city: inputs.city,
-          bedroom: parseInt(inputs.bedroom),
-          bathroom: parseInt(inputs.bathroom),
-          type: inputs.type,
-          property: inputs.property,
-          latitude: inputs.latitude,
-          longitude: inputs.longitude,
-          images: images,
-        },
-        postDetail: {
-          desc: value,
-          utilities: inputs.utilities,
-          pet: inputs.pet,
-          income: inputs.income,
-          size: parseInt(inputs.size),
-          school: parseInt(inputs.school),
-          bus: parseInt(inputs.bus),
-          restaurant: parseInt(inputs.restaurant),
-        },
+      const res = await apiRequest.post("/Property/Create", {
+        title: formData.title,
+        description: value,
+        typeId: parseInt(formData.type),
+        statusId: parseInt(formData.status),
+        startDate: new Date(),
+        endDate: new Date(),
+        price: parseInt(formData.price),
+        currencyId: parseInt(formData.currency),
+        userId: currentUser.userId,
+        longitude: parseFloat(formData.longitude),
+        latitude: parseFloat(formData.latitude),
+        images: null,
       });
-      navigate("/" + res.data.id)
+
+      try {
+        const uploadPromises = images.map((image) =>
+          apiRequest.post("/Photo/Create", {
+            propertyId: res.data.id,
+            photoData: image,
+          })
+        );
+        await Promise.all(uploadPromises);
+      } catch (err) {
+        console.log(err);
+      }
+      navigate("/");
+      console.log(res);
+      //navigate("/" + res.data.id)  TODO EMRE TODO
     } catch (err) {
       console.log(err);
       setError(error);
@@ -61,9 +81,14 @@ function NewPostPage() {
           <form onSubmit={handleSubmit}>
             <div className="item title">
               <label htmlFor="title">Title</label>
-              <input id="title" name="title" type="text" />
+              <input
+                id="title"
+                name="title"
+                type="text"
+                value={formData.title}
+                onChange={handleInputChange}
+              />
             </div>
-
 
             <div className="item description">
               <label htmlFor="desc">Description</label>
@@ -72,33 +97,41 @@ function NewPostPage() {
 
             <div className="item">
               <label htmlFor="price">Price</label>
-              <input id="price" name="price" type="number" />
+              <input
+                id="price"
+                name="price"
+                type="number"
+                value={formData.price}
+                onChange={handleInputChange}
+              />
             </div>
+
             <div className="item">
               <label htmlFor="currency">Currency</label>
               <select
                 name="currency"
                 id="currency"
-                onChange={handleSubmit}
-                defaultValue={currencies[0]}
+                value={formData.currency}
+                onChange={handleInputChange}
               >
                 {currencies.map((currency) => (
-                  <option key={currency.id} value={currency.name}>
+                  <option key={currency.id} value={currency.id}>
                     {currency.name}
                   </option>
                 ))}
               </select>
             </div>
+
             <div className="item">
               <label htmlFor="type">Type</label>
               <select
                 name="type"
                 id="type"
-                onChange={handleSubmit}
-                defaultValue={types[0]}
+                value={formData.type}
+                onChange={handleInputChange}
               >
                 {types.map((type) => (
-                  <option key={type.id} value={type.name}>
+                  <option key={type.id} value={type.id}>
                     {type.name}
                   </option>
                 ))}
@@ -110,30 +143,41 @@ function NewPostPage() {
               <select
                 name="status"
                 id="status"
-                onChange={handleSubmit}
-                defaultValue={statuses[0]}
+                value={formData.status}
+                onChange={handleInputChange}
               >
                 {statuses.map((status) => (
-                  <option key={status.id} value={status.name}>
+                  <option key={status.id} value={status.id}>
                     {status.name}
                   </option>
                 ))}
               </select>
             </div>
 
-
             <div className="item">
               <label htmlFor="latitude">Latitude</label>
-              <input id="latitude" name="latitude" type="text" />
+              <input
+                id="latitude"
+                name="latitude"
+                type="text"
+                value={formData.latitude}
+                onChange={handleInputChange}
+              />
             </div>
+
             <div className="item">
               <label htmlFor="longitude">Longitude</label>
-              <input id="longitude" name="longitude" type="text" />
+              <input
+                id="longitude"
+                name="longitude"
+                type="text"
+                value={formData.longitude}
+                onChange={handleInputChange}
+              />
             </div>
 
-
-            <button className="sendButton">Create New Post</button>
-            {error && <span>error</span>}
+            <button className="sendButton" type="submit">Create New Post</button>
+            {error && <span>{error}</span>}
           </form>
         </div>
       </div>
