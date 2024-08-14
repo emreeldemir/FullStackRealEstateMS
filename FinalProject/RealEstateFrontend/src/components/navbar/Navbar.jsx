1d6ae7fec47fe0f104f85b35e70a4626f76b9475
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-14 19:54:07 +0300

    removed about and contact titles

--- FinalProject/RealEstateFrontend/src/components/navbar/Navbar.jsx
+++ FinalProject/RealEstateFrontend/src/components/navbar/Navbar.jsx
@@ -16,8 +16,8 @@ function Navbar() {
           <span>Emre Estate</span>
         </a>
         <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
+        {/* <a href="/">About</a>
+        <a href="/">Contact</a> */}
       </div>
       <div className="right">
         {currentUser ? (
@@ -45,9 +45,9 @@ function Navbar() {
         </div>
         <div className={open ? "menu active" : "menu"}>
           <a href="/">Home</a>
-          <a href="/">About</a>
+          {/* <a href="/">About</a>
           <a href="/">Contact</a>
-          <a href="/">Agents</a>
+          <a href="/">Agents</a> */}
           <a href="/">Sign in</a>
           <a href="/">Sign up</a>
         </div>
