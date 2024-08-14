86b940896745e7fa6f5a1745fe23a4731484496e
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-15 02:36:44 +0300

    removed console log lines

--- FinalProject/RealEstateFrontend/src/lib/loaders.js
+++ FinalProject/RealEstateFrontend/src/lib/loaders.js
@@ -1,21 +1,14 @@
 import { defer } from "react-router-dom";
-import { useContext } from "react";
 import apiRequest from "./apiRequest";
-import { AuthContext } from "../context/AuthContext";
-
-
 
 export const singlePageLoader = async ({ request, params }) => {
-  const res = await apiRequest("/posts/" + params.id);
-  console.log(res);
+  const res = await apiRequest("/Property/GetPropertyById/" + params.id);
   return res.data;
 };
 
 export const listPageLoader = async ({ request, params }) => {
-  console.log(request.url);
   const query = request.url.split("?")[1];
   const postPromise = await apiRequest("/Property/Search?" + query);
-  console.log(postPromise);
   return defer({
     postResponse: postPromise,
   });
@@ -23,7 +16,6 @@ export const listPageLoader = async ({ request, params }) => {
 
 export const profilePageLoader = async () => {
   const promise = await apiRequest("/Property/GetAllProperties");
-  console.log(promise);
   //const filteredResults = promise.filter(result => result.userId === 8);
 
   return defer({
