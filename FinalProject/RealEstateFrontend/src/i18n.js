f8fb622c0a8a8449c6020d7808896296b209462a
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-15 19:07:39 +0300

    i18n backend imported and translation.json files created for "en" and "tr"

--- FinalProject/RealEstateFrontend/src/i18n.js
+++ FinalProject/RealEstateFrontend/src/i18n.js
@@ -1,30 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
 
-
-const resources = {
-    tr: {
-
-        translation: {
-            home: 'Anasayfa',
-        }
-    },
-
-    en: {
-
-        translation: {
-            home: 'Home',
-        }
-    }
-
-}
 
 
 i18n
     .use(initReactI18next)
+    .use(Backend)
     .init({
         lng: 'en',
-        resources
 
     })
 
