0d2237688e0e85d806a5917aa10983f8b73e48f2
Emre Eldemir <emreeldemir893@gmail.com>
2024-08-15 19:41:36 +0300

    homepage translations implemented

--- FinalProject/RealEstateFrontend/src/routes/homePage/homePage.jsx
+++ FinalProject/RealEstateFrontend/src/routes/homePage/homePage.jsx
@@ -3,18 +3,20 @@ import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 import { AuthContext } from "../../context/AuthContext";
 import { DropdownContextProvider } from "../../context/DropdownContext";
+import { useTranslation } from 'react-i18next';
 
 function HomePage() {
 
   const { currentUser } = useContext(AuthContext)
+  const { t } = useTranslation();
 
   return (
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <h1 className="title">{t('homepage-title')}</h1>
           <p>
-            OBSS .NET CodeCamp 2024 Final Project (Real Estate Management System)
+            {t('homepage-subtitle')}
           </p>
           <DropdownContextProvider>
             <SearchBar />
@@ -22,15 +24,15 @@ function HomePage() {
           <div className="boxes">
             <div className="box">
               <h1>4+</h1>
-              <h2>Years of Experience</h2>
+              <h2>{t('years-of-experience')}</h2>
             </div>
             <div className="box">
               <h1>23</h1>
-              <h2>Award Gained</h2>
+              <h2>{t('award-gained')}</h2>
             </div>
             <div className="box">
               <h1>500+</h1>
-              <h2>Property Ready</h2>
+              <h2>{t('property-ready')}</h2>
             </div>
           </div>
         </div>
